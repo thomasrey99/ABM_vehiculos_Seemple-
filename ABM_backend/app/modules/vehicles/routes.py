d8cdaa65926742e2ece17807a66e8cd98d888bba
thomasrey99 <thomas_rey1999@outlook.com>
2026-08-04 10:40:10 -0300

    fix get by license plate route

--- ABM_backend/app/modules/vehicles/routes.py
+++ ABM_backend/app/modules/vehicles/routes.py
@@ -65,7 +65,7 @@ async def get_all_vehicles(
 ):
     return await controller.get_all()
 
-router.get("/patente/{license_plate}")
+@router.get("/patente/{license_plate}")
 async def get_vehicle_by_license_plate(
     license_plate: str,
     controller: VehicleController = Depends(get_vehicle_controller)
